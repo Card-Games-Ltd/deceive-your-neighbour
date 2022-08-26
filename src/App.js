7bf04d129f9a3a9dac913c972602ed3bfc1d71ba
nik.m <nik.m@investment.com.au>
2022-08-27 03:49:21 +1000

    Add user creation with API calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,12 +11,21 @@ import Room from "./pages/Room";
 import Welcome from "./pages/Welcome";
 
 export default function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+  const addUser = (attributes) => {
+    if (!user) {
+      setUser(attributes);
+      localStorage.setItem('user', JSON.stringify(attributes));
+    }
+  }
+
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/rooms/:id" element={<Room />}></Route>
-          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/" element={<Welcome user={user} addUser={addUser} />}></Route>
         </Routes>
       </div>
     </Router>
