230500998d30a39cc0828407c301d14c29c2c83b
nik.m <nik.m@investment.com.au>
2022-08-26 23:09:19 +1000

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
+import CreateRoom from "./pages/CreateRoom";
+import Room from "./pages/Room";
+import Rooms from "./pages/Rooms";
+import Welcome from "./pages/Welcome";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>{/* ... */}</div>
+
+        <Routes>
+          <Route path="/create">
+            <CreateRoom />
+          </Route>
+          <Route path="/rooms/:id">
+            <Room />
+          </Route>
+          <Route path="/rooms">
+            <Rooms />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
